575e33667e063ee191d1f8dc4c44b27f515ff471
Juan Pedro Lozano <jpyreaction@gmail.com>
2025-01-19 15:16:51 +0100

    Initial Commit

---
+++ StarWarsPlanets/StarWarsPlanetsApp.swift
@@ -0,0 +1,20 @@
+//
+//  StarWarsPlanetsApp.swift
+//  StarWarsPlanets
+//
+//  Created by Juan Pedro Lozano Baño on 19/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsPlanetsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
